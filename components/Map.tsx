59f551127a684f1d9cedbdd2ebca255eaef5170a
Vadim Savin <savinvadim1312@gmail.com>
2024-06-14 17:42:24 +0200

    Render Markers on the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,37 @@
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, {
+  Camera,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+  Images,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+
+import pin from '~/assets/pin.png';
+import scooters from '~/data/scooters.json';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map() {
+  const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
+
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
-      <Camera followZoomLevel={16} followUserLocation />
+      <Camera followZoomLevel={10} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+
+      <ShapeSource id="scooters" shape={featureCollection(points)}>
+        <SymbolLayer
+          id="scooter-icons"
+          style={{
+            iconImage: 'pin',
+            iconSize: 0.5,
+            iconAllowOverlap: true,
+          }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 }
